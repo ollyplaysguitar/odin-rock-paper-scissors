2c4835299905bc8b3148d5488f85803a003654cd
ollyplaysguitar <ollyplaysguitar@gmail.com>
2022-10-21 09:40:53 +0100

    Basic functionality and initial presentation

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # odin-rock-paper-scissors
 
 Odin project: rock paper scissors - console edition.
+
+#images:
+rock papper and scissors by Lucas Helle from https://thenounproject.com/browse/icons/term/rock-papper-and-scissors/"
+
+Sound Effect from <a href="https://pixabay.com/sound-effects/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=6776">Pixabay</a>
+Sound Effect from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=39351">Pixabay</a>
--- script.js
+++ script.js
@@ -1,49 +1,132 @@
+const game = {};
+
+replayBtn = document.createElement("button");
+replayBtn.textContent = "Play Again?";
+replayBtn.classList.add("replay");
+
+const ui = {
+  playArea: document.querySelector(".play-area"),
+  message: document.querySelector(".message"),
+  roundDisplay: document.querySelector(".round-display"),
+  scoreRounds: document.querySelector(".score-round"),
+  scoreWins: document.querySelector(".score-wins"),
+  scoreLosses: document.querySelector(".score-losses"),
+  scoreDraws: document.querySelector(".score-draws"),
+  playerDisplayImg: document.querySelector(".player-display img"),
+  compDisplayImg: document.querySelector(".comp-display img"),
+
+  updateScores: function () {
+    this.scoreRounds.textContent = 5 - game.rounds;
+    this.scoreWins.textContent = game.wins;
+    this.scoreLosses.textContent = game.losses;
+    this.scoreDraws.textContent = game.draws;
+  },
+
+  showRoundResult: function (result) {
+    this.message.textContent = "";
+    if (result === "Draw") {
+      this.message.append(result);
+      return;
+    }
+    ui.message.append(result ? `You won!` : `You lost!`);
+  },
+
+  gameOver: function () {
+    //this.message.textContent = "This is the end!";
+    this.message.textContent = `Game over, Score: ${game.wins}`;
+    startBtn.textContent = "Play Again?";
+    startBtn.classList.remove("hidden");
+    buttons.forEach((btn) => btn.setAttribute("disabled", true));
+  },
+};
+
+const setup = function (rounds) {
+  startBtn.classList.add("hidden");
+  ui.roundDisplay.classList.remove("hidden");
+  ui.message.textContent = "";
+  game.rounds = rounds;
+  game.wins = 0;
+  game.losses = 0;
+  game.draws = 0;
+  game.playerSelection = "";
+  game.computerSelection = "";
+
+  buttons.forEach((btn) => btn.removeAttribute("disabled"));
+  ui.updateScores();
+};
+
+const gameOver = function () {
+  // pass
+  ui.gameOver();
+};
+
 const getComputerChoice = function () {
-  const choices = ["ROCK", "PAPER", "SCISSORS"];
+  const choices = ["Rock", "Paper", "Scissors"];
   const random = Math.floor(Math.random() * 3);
   return choices[random];
 };
 
-const playRound = function (playerSelection, computerSelection) {
-  playerSelection = playerSelection.toUpperCase();
-  // compare first letter to allow for typos
-  if (playerSelection[0] === computerSelection[0]) return "Draw!";
-
-  let winner = false;
-
-  winner = playerSelection[0] === "R" && computerSelection[0] === "S" ? true : winner;
-  winner = playerSelection[0] === "P" && computerSelection[0] === "R" ? true : winner;
-  winner = playerSelection[0] === "S" && computerSelection[0] === "P" ? true : winner;
-
-  return winner;
+const sleep = function (ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
-const game = function (rounds) {
-  let playerChoice, computerChoice;
-  let score = 0;
-  let draws = 0;
+const playRound = async function (playerSelection, computerSelection) {
+  ui.compDisplayImg.src = `images/question.png`;
+  game.rounds--;
+  ui.updateScores();
+  game.playerSelection = playerSelection;
+  game.computerSelection = computerSelection;
 
-  for (let i = 1; i <= rounds; i++) {
-    console.log("ROUND ", i);
+  ui.playerDisplayImg.src = `images/${playerSelection}.png`;
 
-    playerChoice = prompt("Select : Rock, Paper or Scissors?");
-    computerChoice = getComputerChoice();
-    const roundResult = playRound(playerChoice, computerChoice);
+  ui.message.textContent = "3";
+  await sleep(300);
+  ui.message.textContent = "2";
+  await sleep(300);
+  ui.message.textContent = "1";
+  await sleep(300);
 
-    if (roundResult === "Draw!") {
-      console.log(`It was a draw! You both selected ${computerChoice}`);
-      draws++;
-    } else {
-      console.log(roundResult ? `You get a point! Computer selected ${computerChoice}` : `Oh no! Computer got a point by selecting ${computerChoice}`);
-      score = roundResult ? score + 1 : score;
-    }
+  ui.compDisplayImg.src = `images/${computerSelection}.png`;
+
+  if (playerSelection === computerSelection) {
+    game.draws++;
+    ui.showRoundResult("Draw");
+  } else {
+    let winner = false;
+
+    winner = playerSelection === "Rock" && computerSelection === "Scissors" ? true : winner;
+    winner = playerSelection === "Paper" && computerSelection === "Rock" ? true : winner;
+    winner = playerSelection === "Scissors" && computerSelection === "Paper" ? true : winner;
+
+    if (winner) game.wins++;
+    if (!winner) game.losses++;
+    console.log("wins: ", game.wins, " losses: ", game.losses, " draws: ", game.draws);
+    ui.showRoundResult(winner);
   }
-  const endMsg = `The Final Score was ${score} Points to you and ${rounds - score - draws} Points for the Computer`;
-  if (score === rounds - score - draws) {
-    console.log(`It was a draw! -- ${endMsg}`);
+
+  ui.updateScores();
+
+  if (game.rounds <= 0) {
+    gameOver();
     return;
   }
-  console.log(score > rounds - score - draws ? `You Win! -- ${endMsg}` : `You Lose! -- ${endMsg}`);
 };
 
-game(7);
+const startBtn = document.querySelector(".play-area button");
+const buttons = document.querySelectorAll("button[data-rps]");
+
+startBtn.addEventListener("click", function () {
+  setup(5);
+});
+
+ui.playArea.addEventListener("click", function (e) {
+  if (e.target.classList.contains("replay")) {
+    setup(5);
+  }
+});
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    playRound(e.target.dataset.rps, getComputerChoice());
+  });
+});
